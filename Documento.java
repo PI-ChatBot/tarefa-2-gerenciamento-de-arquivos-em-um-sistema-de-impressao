9b75cc5166477829b3428e0fb131609475306166
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-05-08 10:20:19 -0300

    feat: criação da classe Documento

---
+++ Documento.java
@@ -0,0 +1,10 @@
+import java.time.LocalDateTime;
+
+/**
+ * Documento que será impresso.
+ */
+public class Documento {
+    private String nomeArquivo;
+    private String nomeUsuario;
+    private LocalDateTime horarioSolicitacao;
+}
